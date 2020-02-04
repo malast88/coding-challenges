a23ab55ff78b26b611bfbd71e3dc09e74d78ebe1
malast <malast88@gmail.com>
2020-02-04 20:08:09 +0100

    add Roman to Integer

---
+++ src/test/java/io/github/malast88/coding/leetcode/RomanToIntegerTest.java
@@ -0,0 +1,34 @@
+package io.github.malast88.coding.leetcode;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanToIntegerTest {
+
+    private static Stream<Arguments> getGetRomanIntegerArgs() {
+        return Stream.of(
+                Arguments.of("III", 3),
+                Arguments.of("IV", 4),
+                Arguments.of("MCMCDXLVIV", 2349),
+                Arguments.of("BIV", 4)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getGetRomanIntegerArgs")
+    public void itShouldGetRomanInteger(String s, int expected) {
+        // arrange
+        RomanToInteger sut = new RomanToInteger();
+
+        // act
+        int actual = sut.romanToInt(s);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+}
