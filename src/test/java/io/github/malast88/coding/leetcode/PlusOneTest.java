2b4a5ec9e0966476f641b5dbc59fd30f1d214191
malast <malast88@gmail.com>
2020-02-23 22:04:11 +0100

    add Plus one

---
+++ src/test/java/io/github/malast88/coding/leetcode/PlusOneTest.java
@@ -0,0 +1,33 @@
+package io.github.malast88.coding.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class PlusOneTest {
+
+  private static Stream<Arguments> geCountAndSayArgs() {
+    return Stream.of(
+        Arguments.of(new int[] {0}, new int[] {1}),
+        Arguments.of(new int[] {1, 2, 3, 4}, new int[] {1, 2, 3, 5}),
+        Arguments.of(new int[] {9, 8, 9}, new int[] {9, 9, 0}),
+        Arguments.of(new int[] {9}, new int[] {1, 0}),
+        Arguments.of(new int[] {9, 9, 9}, new int[] {1, 0, 0, 0}));
+  }
+
+  @ParameterizedTest
+  @MethodSource("geCountAndSayArgs")
+  public void itShouldCountAndSay(int[] digits, int[] expected) {
+    // arrange
+    PlusOne sut = new PlusOne();
+
+    // act
+    int[] actual = sut.plusOne(digits);
+
+    // assert
+    assertArrayEquals(expected, actual);
+  }
+}
